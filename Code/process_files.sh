e5fb6615cef8d99f8ef659591df76b7bf7e85303
timoast <timstuart90@gmail.com>
2016-10-28 13:58:18 +0800

    first commit

---
+++ Code/process_files.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+# move each file to it's own folder
+# move each folder to scratch
+# remove duplicates
+# index
+# move back to working_data
+
+wd=`pwd`
+
+for myfile in ./*.bam; do
+    fname=(${myfile//.bam/ })
+    mkdir $fname
+    mv $myfile $fname
+    mv $fname /home/tstuart/scratch
+    cd /home/tstuart/scratch/$fname
+    picard MarkDuplicates I=$myfile O=$fname.rmdup.bam M=dup_metrics.txt \
+	   REMOVE_DUPLICATES=true
+    samtools index $fname.rmdup.bam
+    cd ..
+    mv $fname 
+    cd $wd
+done
+
---
+++ README.md
@@ -0,0 +1,9 @@
+# Salt stress ATAC-seq
+
+Salt stress was performed in duplicate on Arabidopsis plants
+expressing the INTACT construct for isolation of different cell
+types. Mannitol was used as a control for changes in osmotic pressure
+during salt stress. ATAC-seq was performed on root cell types from
+stressed or control plants.
+
+
