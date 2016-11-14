017bf80e1b473105ab08eac4952d346c2f2973d6
timoast <timstuart90@gmail.com>
2016-11-14 10:55:56 +0800

    Clean up code for bigwig files

--- Code/bamToBedpe.sh
+++ Code/bamToBedpe.sh
@@ -7,12 +7,8 @@ bam="$1"
 cores="$2"
 prefix=$(echo "$bam" | sed 's/.bam//g')
 
-
-# Remove duplicates
-# sambamba markdup -r -t "$cores" "$bam" "$prefix".rmdup.bam
-
 # Sort bam file by read name
-samtools sort -n -@ "$cores" "$prefix".rmdup.bam > "$prefix".sorted.bam
+samtools sort -n -@ "$cores" -T temp -O bam "$prefix".rmdup.bam > "$prefix".sorted.bam
 
 # Update/fix flags
 samtools fixmate "$prefix".sorted.bam "$prefix".fixed.bam
@@ -25,9 +21,6 @@ sortBed -i "$prefix".tmp.bedpe | awk -v OFS="\t" '{if ( !($1==$4) || $9==$10 || 
 
 # Offset (+5 for + mapped reads, -4 for - mapped reads)
 awk -v OFS="\t" '{if ($9=="+") print $1, $2+4, $3+4, $4, $5-5, $6-5, $7,$8,$9,$10; else print $1, $2-5,$3-5, $4, $5+4, $6+4, $7,$8,$9,$10}' "$prefix".filter.bedpe > "$prefix".bedpe
-
-
-
 
 # Remove tmp files
 rm "$prefix".sorted.bam "$prefix".fixed.bam "$prefix".tmp.bedpe "$prefix".filter.bedpe
--- Code/insertion_coverage.sh
+++ Code/insertion_coverage.sh
@@ -7,12 +7,9 @@ bedpe="$1"
 prefix=$(echo "$bedpe" | sed 's/.bedpe//g')
 
 # Create bed file from bedpe
-
 cut -f 1-2,6-9 "$bedpe" > "$prefix".bed
 
-
 # Create 300pb reads centered on each Tn5 insertion event (5' and 3' of paired-end reads)
-
 awk -v OFS="\t" '{if ($2>50) print $1,$2-150,$2+149 RS $1,$3-149,$3+150; else print $1,$3-149,$3+150}' "$prefix".bed > "$prefix"_insertion.tmp.bed
 
 bedClip "$prefix"_insertion.tmp.bed /home/moliva/working_data/genomes/TAIR10/chromosomesize.txt "$prefix"_insertion.fixed.tmp.bed
@@ -21,7 +18,6 @@ sort -k1,1 -k2,2n -k3,3n "$prefix"_insertion.fixed.tmp.bed > "$prefix"_insertion
 
 
 # Calculate coverage for Tn5 insertions
-
 scalingFactor=$(cat "$prefix"_insertion.bed | awk 'END {print 1000000/NR}')
 
 bedtools genomecov -bg \
@@ -30,11 +26,6 @@ bedtools genomecov -bg \
 -g /home/moliva/working_data/genomes/TAIR10/chromosomesize.txt > "$prefix"_insertion.bedgraph
 
 # Create BigWig file
-
 bedGraphToBigWig "$prefix"_insertion.bedgraph /home/moliva/working_data/genomes/TAIR10/chromosomesize.txt "$prefix"_insertion.bigwig
 
-
-
-
 rm "$prefix"_insertion.tmp.bed "$prefix"_insertion.fixed.tmp.bed
-
